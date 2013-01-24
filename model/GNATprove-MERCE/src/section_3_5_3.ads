039cf629704b9d0732c440cbe9bb65cb3ca43fac
David MENTRE <d.mentre@fr.merce.mee.com>
2013-01-24 17:18:43 +0100

    Put all files in UTF-8 encoding
    Signed-off-by: David MENTRE <d.mentre@fr.merce.mee.com>

--- model/GNATprove-MERCE/src/section_3_5_3.ads
+++ model/GNATprove-MERCE/src/section_3_5_3.ads
@@ -28,7 +28,7 @@ with Com_Map;
 use Com_Map;
 
 Package Section_3_5_3 is
-   -- ง3.5.3.4
+   -- ยง3.5.3.4
    Start_Of_Mission : Boolean;
    Track_Side_New_Communication_Order : Boolean;
    Mode_Change_Report_To_RBC_Not_Considered_As_End_Of_Mission : Boolean; -- to be refined
@@ -43,39 +43,39 @@ Package Section_3_5_3 is
 
    function Authorize_New_Communication_Session return Boolean is
      ((Start_Of_Mission = True
-       and (ertms_etcs_level = 2 or ertms_etcs_level = 3)) -- ง3.5.3.4.a
-      and Track_Side_New_Communication_Order = True -- ง3.5.3.4.b
+       and (ertms_etcs_level = 2 or ertms_etcs_level = 3)) -- ยง3.5.3.4.a
+      and Track_Side_New_Communication_Order = True -- ยง3.5.3.4.b
       and (Mode_Change_Report_To_RBC_Not_Considered_As_End_Of_Mission = True
-           and (ertms_etcs_level = 2 or ertms_etcs_level = 3))-- ง3.5.3.4.c
+           and (ertms_etcs_level = 2 or ertms_etcs_level = 3))-- ยง3.5.3.4.c
       and (Manual_Level_Change = True
-           and (ertms_etcs_level = 2 or ertms_etcs_level = 3)) -- ง3.5.3.4.d
-      and Train_Front_Reaches_End_Of_Radio_Hole = True -- ง3.5.3.4.e
-      and Previous_Communication_Loss = True -- ง3.5.3.4.f
+           and (ertms_etcs_level = 2 or ertms_etcs_level = 3)) -- ยง3.5.3.4.d
+      and Train_Front_Reaches_End_Of_Radio_Hole = True -- ยง3.5.3.4.e
+      and Previous_Communication_Loss = True -- ยง3.5.3.4.f
       and (Start_Of_Mission_Procedure_Completed_Without_Com = True
-           and (ertms_etcs_level = 2 or ertms_etcs_level = 3)) -- ง3.5.3.4.g
+           and (ertms_etcs_level = 2 or ertms_etcs_level = 3)) -- ยง3.5.3.4.g
      );
 
-   -- ง3.5.3.1 and ง3.5.3.2 implicitly fulfilled as we model on-board
+   -- ยง3.5.3.1 and ยง3.5.3.2 implicitly fulfilled as we model on-board
    procedure Initiate_Communication_Session(destination : RBC_RIU_ID_t)
    with
-     Pre => ((Authorize_New_Communication_Session = True) -- ง3.5.3.4
-             and (not Connections.Contains(destination)) -- ง3.5.3.4.1
+     Pre => ((Authorize_New_Communication_Session = True) -- ยง3.5.3.4
+             and (not Connections.Contains(destination)) -- ยง3.5.3.4.1
              -- FIXME: what should we do for cases f and g?
             ),
      Post => (Connections.Contains(destination));
 
-   -- ง3.5.3.3 not formalized (Note)
+   -- ยง3.5.3.3 not formalized (Note)
 
    type Telephone_Number_t is range 0..20_000; -- FIXME refine range
    type RBC_Contact_Action_t is (Establish_Session, Terminate_Session);
 
-   -- ง3.5.3.5
+   -- ยง3.5.3.5
    procedure Contact_RBC(RBC_identity : RBC_RIU_ID_t;
                          RBC_number : Telephone_Number_t;
                          Action : RBC_Contact_Action_t;
                          Apply_To_Sleeping_Units : Boolean);
 
-   -- ง3.5.3.5.1 to be formalized. The content of table ง3.5.3.16 should be
+   -- ยง3.5.3.5.1 to be formalized. The content of table ยง3.5.3.16 should be
    -- incorporated as above operation post-condition (if possible)
 
 
