7af9189c2db69be27da8f83e08a4b0ac94b23c3e
David MENTRE <d.mentre@fr.merce.mee.com>
2013-03-27 16:13:19 +0100

    Test Step_Function.Has_Same_Delimiters
    Signed-off-by: David MENTRE <d.mentre@fr.merce.mee.com>

--- model/GNATprove-MERCE/src/step_function_test.adb
+++ model/GNATprove-MERCE/src/step_function_test.adb
@@ -7,6 +7,13 @@ procedure Step_Function_Test is
       Step => ((Delimiter => 3, Value => 2.0),
                (Delimiter => 5, Value => 5.0),
                others => (Delimiter => 0, Value => 0.0)));
+
+   SFun2 : Step_Function.Step_Function :=
+     (Default_Value => 1.0,
+      Number_Of_Delimiters => 2,
+      Step => ((Delimiter => 3, Value => 1.0),
+               (Delimiter => 5, Value => 3.0),
+               others => (Delimiter => 0, Value => 0.0)));
 begin
    Pragma Assert (Step_Function.Is_Valid_Step_Function(SFun1));
 
@@ -17,4 +24,6 @@ begin
    Pragma Assert (Step_Function.Get_Value(SFun1, 5) = 5.0);
    Pragma Assert (Step_Function.Get_Value(SFun1,
      Step_Function.Function_Range'Last) = 5.0);
+
+   Pragma Assert (Step_Function.Has_Same_Delimiters(SFun1, SFun2));
 end;
